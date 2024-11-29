e7e7a40992a52e67e5f80ec39d22749ce74cb720
Afonso Tinoco <afonso.tfaria21@gmail.com>
2024-11-28 23:47:57 -0500

    Investigate runner related benchmark issue

--- crates/storage/benches/backend.rs
+++ crates/storage/benches/backend.rs
@@ -22,10 +22,35 @@ pub fn benchmark_storage<T: Measurement + 'static>(c: &mut Criterion<T>) {
     })
   });
 
+  c.bench_function("mov-40k", |b| {
+    let src: Vec<u8> = vec![0; 4096 * 10];
+    let mut dst: Vec<u8> = vec![0; 4096 * 10];
+    b.iter(|| {
+      dst.copy_from_slice(black_box(&src));
+    })
+  });
+
+  c.bench_function("memstore-40k", |b| {
+    let src = MemStore::open(String::new(), 10).unwrap();
+    let mut dst: Vec<u8> = vec![0; 4096];
+    b.iter(|| {
+      black_box(src.read_page(black_box(0), black_box(&mut dst))).unwrap();
+      black_box(src.read_page(black_box(1), black_box(&mut dst))).unwrap();
+      black_box(src.read_page(black_box(2), black_box(&mut dst))).unwrap();
+      black_box(src.read_page(black_box(3), black_box(&mut dst))).unwrap();
+      black_box(src.read_page(black_box(4), black_box(&mut dst))).unwrap();
+      black_box(src.read_page(black_box(5), black_box(&mut dst))).unwrap();
+      black_box(src.read_page(black_box(6), black_box(&mut dst))).unwrap();
+      black_box(src.read_page(black_box(7), black_box(&mut dst))).unwrap();
+      black_box(src.read_page(black_box(8), black_box(&mut dst))).unwrap();
+      black_box(src.read_page(black_box(9), black_box(&mut dst))).unwrap();
+    })
+  });
+
   // NOTE: mov-4k and memstore-4k should have similar performance.
 }
 
 criterion_group!(name = benches_cycles;
-  config = Criterion::default().with_measurement(CyclesPerByte).warm_up_time(std::time::Duration::from_millis(2000)).measurement_time(std::time::Duration::from_secs(3));
+  config = Criterion::default().with_measurement(CyclesPerByte).warm_up_time(std::time::Duration::from_millis(500)).measurement_time(std::time::Duration::from_secs(1));
   targets = benchmark_storage);
 criterion_main!(benches_cycles);
