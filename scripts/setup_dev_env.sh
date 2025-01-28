0a5de217a0dde9ff270a71a4c7381bc29ded7679
Afonso Tinoco <afonso.tfaria21@gmail.com>
2025-01-28 11:18:28 -0500

    Add prepush hook

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -4,3 +4,6 @@ cargo install cargo-criterion
 cargo install cargo-hack
 cargo install cargo-nextest
 cargo install zepter
+
+echo "cargo make precommit" > .git/hooks/pre-push
+chmod +x .git/hooks/pre-push
