235d1a9ac0d2f60b99c3899773d15422ae2fc03c
GUA <songjunxi@GUA.local>
2025-01-28 13:37:57 -0800

    include codespell in setup_dev_env

--- scripts/setup_dev_env.sh
+++ scripts/setup_dev_env.sh
@@ -8,3 +8,11 @@ cargo install cargo-make
 
 echo "cargo make precommit" > .git/hooks/pre-push
 chmod +x .git/hooks/pre-push
+
+# Check if Homebrew is installed, and install codespell if it is
+if command -v brew &> /dev/null; then
+    echo "Homebrew is installed. Installing codespell..."
+    brew install codespell
+else
+    echo "Homebrew is not installed. Skipping codespell installation. You may need to manually install codespell to push"
+fi
