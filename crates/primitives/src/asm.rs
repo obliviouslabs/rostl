c7e814be788a6a4a0be6d49f459ee2be3ca08d02
Afonso Tinoco <afonso.tfaria21@gmail.com>
2025-08-02 11:43:44 -0400

    Fix failing tests due to rust sse2 bug

--- crates/primitives/src/asm.rs
+++ crates/primitives/src/asm.rs
@@ -176,19 +176,21 @@ macro_rules! cxchg_body {
       }
     }
 
-    #[cfg(target_feature = "sse2")]
-    {
-      // Process in chunks of 16 bytes (u128)
-      while i + 16 <= self_bytes.len() {
-        let self_chunk = &mut self_bytes[i..i + 8];
-        let other_chunk = &mut other_bytes[i..i + 8];
-        let self_u128 = unsafe { &mut *(self_chunk.as_mut_ptr() as *mut u128) };
-        let other_u128 = unsafe { &mut *(other_chunk.as_mut_ptr() as *mut u128) };
 
-        self_u128.cxchg_base(other_u128, $choice);
-        i += 16;
-      }
-    }
+    // UNDONE(): This seems to be broken in rust 1.89:
+    // #[cfg(target_feature = "sse2")]
+    // {
+    //   // Process in chunks of 16 bytes (u128)
+    //   while i + 16 <= self_bytes.len() {
+    //     let self_chunk = &mut self_bytes[i..i + 16];
+    //     let other_chunk = &mut other_bytes[i..i + 16];
+    //     let self_u128 = unsafe { &mut *(self_chunk.as_mut_ptr() as *mut u128) };
+    //     let other_u128 = unsafe { &mut *(other_chunk.as_mut_ptr() as *mut u128) };
+
+    //     self_u128.cxchg_base(other_u128, $choice);
+    //     i += 16;
+    //   }
+    // }
 
 
     // Process in chunks of 8 bytes (u64)
